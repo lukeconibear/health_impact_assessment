cd615ac1b169c075add7b0dd70f3d850191b9641
Luke Conibear <l.a.conibear@leeds.ac.uk>
2020-07-09 16:02:11 +0100

    Update example_regridding.py

--- example_regridding.py
+++ example_regridding.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python3
+"""
+Create daily mean surface ambient PM2.5 concentrations from raw WRFChem wrfout files
+Regrid to a global regrid
+"""
+import glob
+import numpy as np
 import xarray as xr
 import xesmf as xe
 import salem
 
-with salem.xr.open_dataset('/path/wrf.nc') as ds:
-    wrf = ds['variable']
+path = '/nfs/b0122/Users/earlacoa/test/'
+wrfout_files = sorted(glob.glob(path + '/wrfout_*'))
+
+# combine pm25 from multiple wrfout files together
+with salem.open_mf_wrf_dataset(wrfout_files, chunks={'west_east':'auto', 'south_north':'auto'}) as ds:
+    wrf_pm25 = ds['PM2_5_DRY']
     
+wrf_pm25_surface = wrf_pm25.isel(bottom_top=0)
+wrf_pm25_surface_dailymean = wrf_pm25_surface.resample(time='24H').mean()
+
 global_grid = xr.Dataset(
     {'lat': (['lat'], np.arange(-60, 85, 0.0416667)), 
      'lon': (['lon'], np.arange(-180, 180, 0.0416667)),}
 )
 
 regridder = xe.Regridder(
-    wrf, 
+    wrf_pm25_surface_dailymean, 
     global_grid, 
-    'bilinear', 
-    reuse_weights=True
+    'bilinear'
 )
 
-wrf_regridded = regridder(wrf)
-
-regridder.clean_weight_file()
+wrf_pm25_surface_dailymean_regridded = regridder(wrf_pm25_surface_dailymean)
