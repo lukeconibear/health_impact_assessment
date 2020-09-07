81d45a031035853a1e92caca61b0e82c3294b038
Luke Conibear <l.a.conibear@leeds.ac.uk>
2020-09-07 18:32:18 +0100

    making more generic

--- example_crop_array_to_shapefile.py
+++ example_crop_array_to_shapefile.py
@@ -6,20 +6,19 @@ from cutshapefile import transform_from_latlon, rasterize
 import geopandas as gpd
 
 # load shapefile (single multipolygon) and extract shapes
-shapefile_china = gpd.read_file('/nobackup/earlacoa/emissions/shapefiles/CHN_adm0.shp')
-shapes_china = [(shape, index) for index, shape in enumerate(shapefile_china.geometry)]
+shapefile = gpd.read_file('/nobackup/earlacoa/emissions/shapefiles/CHN_adm0.shp')
+shapes = [(shape, index) for index, shape in enumerate(shapefile.geometry)]
 
 # load wrf dataset
 ds = salem.xr.open_dataset('/path/wrf.nc')
 
 # apply shapefile to geometry, default: inside shapefile == 0, outside shapefile == np.nan
-ds['china'] = rasterize(shapes_china, ds.coords, longitude='lon', latitude='lat') 
+ds['shapefile'] = rasterize(shapes, ds.coords, longitude='lon', latitude='lat') 
 
 # change to more intuitive labelling of 1 for inside shapefile and np.nan for outside shapefile
-# if condition preserve (outside china, as inside defaults to 0), otherwise (1, to mark in china)
-ds['china'] = ds.china.where(cond=ds.china!=0, other=1) 
+# if condition preserve (outside shapefile, as inside defaults to 0), otherwise (1, to mark in shapefile)
+ds['shapefile'] = ds.shapefile.where(cond=ds.shapefile!=0, other=1) 
 
 # example: scale data inside shapefile by 0.5
-# if condition (not in china) preserve, otherwise (in china, and scale)
-ds = ds.where(cond=ds.china!=1, other=ds*0.5) 
-
+# if condition (not in shapefile) preserve, otherwise (in shapefile, and scale)
+ds = ds.where(cond=ds.shapefile!=1, other=ds*0.5) 
